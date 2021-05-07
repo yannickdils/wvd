4e17c844a6d295de7da1402b16fe4d1dab63beee
Yannick Dils <yannick.dils@tunecom.be>
2021-05-07 12:25:42 +0200

    Removed duplicate entries

--- ImageManagement/OptimizeLocalVHDImage.ps1
+++ ImageManagement/OptimizeLocalVHDImage.ps1
@@ -77,81 +77,6 @@ function PrepareVM {
 
 }
 
-function ValidateVM {
-      
-    Write-host "Performing Windows File System Check"
-    sfc.exe /scannow
-    
-    Write-host "Displaying Persistent Routes"
-    $routes = route.exe print
-    
-    Write-Host "Removing Static Routes"
-    route.exe delete *
-    
-    Write-Host "Removing WinHTTP proxy"
-    netsh.exe winhttp reset proxy
-    
-    Write-Host "Configuring SAN policy"
-    start-process diskpart.exe -ArgumentList "san policy=onlineall  exit"
-    
-    Write-Host "Configuring time zone"
-    Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation -Name RealTimeIsUniversal -Value 1 -Type DWord -Force
-    Set-Service -Name w32time -StartupType Automatic
-    
-    Write-Host "Set powerprofile to high performance"
-    powercfg.exe /setactive SCHEME_MIN
-    
-    Write-Host "Setting default temp variables"
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -Name TEMP -Value "%SystemRoot%\TEMP" -Type ExpandString -Force
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -Name TMP -Value "%SystemRoot%\TEMP" -Type ExpandString -Force
-    
-    Write-host "Checking windows services"
-    Get-Service -Name BFE, Dhcp, Dnscache, IKEEXT, iphlpsvc, nsi, mpssvc, RemoteRegistry | Where-Object StartType -ne Automatic | Set-Service -StartupType Automatic
-    Get-Service -Name Netlogon, Netman, TermService | Where-Object StartType -ne Manual | Set-Service -StartupType Manual
-    
-    Write-Host "Setting remote access settings"
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name fDenyTSConnections -Value 0 -Type DWord -Force
-    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name fDenyTSConnections -Value 0 -Type DWord -Force
-    
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -Name PortNumber -Value 3389 -Type DWord -Force
-    
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -Name LanAdapter -Value 0 -Type DWord -Force
-    
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name UserAuthentication -Value 1 -Type DWord -Force
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name SecurityLayer -Value 1 -Type DWord -Force
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name fAllowSecProtocolNegotiation -Value 1 -Type DWord -Force
-    
-    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name KeepAliveEnable -Value 1  -Type DWord -Force
-    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name KeepAliveInterval -Value 1  -Type DWord -Force
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -Name KeepAliveTimeout -Value 1 -Type DWord -Force
-    
-    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name fDisableAutoReconnect -Value 0 -Type DWord -Force
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -Name fInheritReconnectSame -Value 1 -Type DWord -Force
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -Name fReconnectSame -Value 0 -Type DWord -Force
-    
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -Name MaxInstanceCount -Value 4294967295 -Type DWord -Force
-    
-    if ((Get-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp').Property -contains 'SSLCertificateSHA1Hash') {
-        Remove-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name SSLCertificateSHA1Hash -Force
-    }
-    
-    Write-Host "Configuring Firewall Rules"
-    
-    Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled True
-    
-    Enable-PSRemoting -Force
-    Set-NetFirewallRule -DisplayName 'Windows Remote Management (HTTP-In)' -Enabled True
-    
-    Set-NetFirewallRule -DisplayGroup 'Remote Desktop' -Enabled True
-    
-    Set-NetFirewallRule -DisplayName 'File and Printer Sharing (Echo Request - ICMPv4-In)' -Enabled True
-    
-    New-NetFirewallRule -DisplayName AzurePlatform -Direction Inbound -RemoteAddress 168.63.129.16 -Profile Any -Action Allow -EdgeTraversalPolicy Allow
-    New-NetFirewallRule -DisplayName AzurePlatform -Direction Outbound -RemoteAddress 168.63.129.16 -Profile Any -Action Allow
-    
-    
-}
-
 function FinalCheckVM {
     
     
@@ -202,8 +127,6 @@ function FinalCheckVM {
 
 Write-Host "Starting Prepare VM phase" -ForegroundColor Green
 PrepareVM
-Write-Host "Starting Validate VM phase" -ForegroundColor
-ValidateVM
 Write-Host "Starting Final Check VM phase" -ForegroundColor Green
 FinalCheckVM
 Write-Host "Script complete ready for sysprep" -ForegroundColor Green
